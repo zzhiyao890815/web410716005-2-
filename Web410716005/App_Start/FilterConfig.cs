c720d80408e0b2cadff5704752b51009751781f2
410716005 <K6628901@gmail.com>
2021-05-06 20:12:40 +0800

    first commit

---
+++ Web410716005/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Web410716005
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
