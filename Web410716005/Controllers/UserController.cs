61ca7a321b772d2028fd3cf825ef8743d0306110
zzhiyao890815 <K6628901@gmail.com.tw>
2021-06-03 20:54:47 +0800

    最後一次作業

--- Web410716005/Controllers/UserController.cs
+++ Web410716005/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web410716005.ViewModels;
 
 namespace Web410716005.Controllers
 {
@@ -12,28 +13,28 @@ namespace Web410716005.Controllers
         // GET: User
         public ActionResult SignUp()
         {
-            return View();
+            return View(new SignUpData());
         }
         [HttpPost]
-        public ActionResult SignUp(string name,string account,string password)
+        public ActionResult SignUp(SignUpData data)
         {
-            if (name.IsNullOrWhiteSpace())
+            if (data.Name.IsNullOrWhiteSpace())
             {
-                ViewBag.NameMessage = "請輸入姓名";
+                data.NameMessage = "請輸入姓名";
             }
-            if (account.IsNullOrWhiteSpace())
+            if (data.Account.IsNullOrWhiteSpace())
             {
-                ViewBag.AccountMessage = "請輸入帳號";
+                data.AccountMessage = "請輸入帳號";
             }
-            if (password.IsNullOrWhiteSpace())
+            if (data.Password.IsNullOrWhiteSpace())
             {
-                ViewBag.PasswordMessage = "請輸入密碼";
+                data.PasswordMessage = "請輸入密碼";
             }
-            if (!name.IsNullOrWhiteSpace() && !account.IsNullOrWhiteSpace() && !password.IsNullOrWhiteSpace())
+            if (!data.Name.IsNullOrWhiteSpace() && !data.Account.IsNullOrWhiteSpace() && !data.Password.IsNullOrWhiteSpace())
             {
-                ViewBag.Message = "註冊成功";
+                data.Message = "註冊成功";
             }
-            return View();
+            return View(data);
         }
     }
 }
---
+++ Web410716005/ViewModels/SignUpData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web410716005.ViewModels
+{
+    public class SignUpData
+    {
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string NameMessage { get; set; }
+        public string AccountMessage { get; set; }
+        public string PasswordMessage { get; set; }
+        public string Message { get; set; }
+
+    }
+}
