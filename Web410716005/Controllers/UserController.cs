e3960d49bed3376b906925d734ed7384b9853411
zzhiyao890815 <K6628901@gmail.com.tw>
2021-06-10 20:51:01 +0800

    student

--- Web410716005/Controllers/UserController.cs
+++ Web410716005/Controllers/UserController.cs
@@ -18,19 +18,7 @@ namespace Web410716005.Controllers
         [HttpPost]
         public ActionResult SignUp(SignUpData data)
         {
-            if (data.Name.IsNullOrWhiteSpace())
-            {
-                data.NameMessage = "請輸入姓名";
-            }
-            if (data.Account.IsNullOrWhiteSpace())
-            {
-                data.AccountMessage = "請輸入帳號";
-            }
-            if (data.Password.IsNullOrWhiteSpace())
-            {
-                data.PasswordMessage = "請輸入密碼";
-            }
-            if (!data.Name.IsNullOrWhiteSpace() && !data.Account.IsNullOrWhiteSpace() && !data.Password.IsNullOrWhiteSpace())
+            if (ModelState.IsValid)
             {
                 data.Message = "註冊成功";
             }
--- Web410716005/ViewModels/SignUpData.cs
+++ Web410716005/ViewModels/SignUpData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,21 @@ namespace Web410716005.ViewModels
 {
     public class SignUpData
     {
+        [Display(Name = "姓名")]
+        [Required(ErrorMessage = "請輸入姓名")]
+        [MinLength(5,ErrorMessage = "請輸入5~10個字")]
+        [MaxLength(10, ErrorMessage = "請輸入5~10個字")]
         public string Name { get; set; }
+
+        [Display(Name = "帳號")]
+        [Required(ErrorMessage = "請輸入帳號")]
+        [EmailAddress(ErrorMessage = "請輸入正確的Email")]
         public string Account { get; set; }
+
+        [Display(Name = "密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        [StringLength(10,MinimumLength = 5,ErrorMessage = "請輸入密碼")]
+
         public string Password { get; set; }
         public string NameMessage { get; set; }
         public string AccountMessage { get; set; }
